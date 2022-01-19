9c2fdf628c85523886a9ec9fbc7f774c8a954365
playdog-io <cqdxcs3@163.com>
2022-01-19 14:33:24 +0800

    使用web3.js连接metamask从而调用智能合约的示例

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,124 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>此页面用于测试前端通过web3.js与metamask相连从而调用智能合约</div>
+    <div>请使用以太坊测试网rinkeby参与测试</div>
+    <div>点击add，成功发送交易后将使存储数据加1，点击reduce，成功发送交易后将使数据减1</div>
+    <div>
+    您的账号地址为：  {{ accounts }}
+    </div>
+
+    <div>
+   当前存储数据为：   {{ number }}
+    </div>
+
+    <button v-on:click="add()">add</button>
+
+    <button v-on:click="reduce()">reduce</button>
   </div>
 </template>
 
 <script>
+const Web3 = require("web3");
+import ABIJson from "./StorageABI.json";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+
+  data() {
+    return {
+      title: "jiba",
+      accounts: "",
+      contract: null,
+      number: 0,
+    };
+  },
+
+  onload() {},
+  async mounted() {
+    let that = this;
+
+    let P = new Promise((resolve, reject) => {
+      const timer = setInterval(async () => {
+        var web3Provider;
+        if (window.ethereum) {
+          web3Provider = window.ethereum;
+          try {
+            const accounts = await window.ethereum.request({
+              method: "eth_requestAccounts",
+            });
+            var web3js = new Web3(web3Provider);
+            web3js.eth.defaultAccount = accounts[0];
+            clearInterval(timer);
+            console.dir(accounts);
+            resolve({ web3: web3js, loggedIn: true });
+          } catch (error) {
+            console.error("User denied account access");
+          }
+        } else if (window.web3) {
+          web3Provider = window.web3.currentProvider;
+          var web3js2 = new Web3(web3Provider);
+          web3js2.eth.getAccounts(function (error, result) {
+            if (error) {
+              console.log(error);
+              reject("User denied account access", false);
+            }
+            console.dir(result);
+            web3js2.eth.defaultAccount = result[0];
+            clearInterval(timer);
+            resolve({ web3: web3js2, loggedIn: true });
+          });
+        }
+      }, 100);
+    });
+    P.then(async (data) => {
+      that.accounts = data.web3.eth.defaultAccount;
+      that.contract = new data.web3.eth.Contract(
+        ABIJson,
+        "0x191a1c48431160cf4f0e86cb7ac868b81555f493"
+      );
+      that.number = Number(await that.contract.methods.retrieve().call());
+      console.dir(that.number);
+    }).catch((e) => {
+      that.accounts = e;
+    });
+  },
+  methods: {
+    async add() {
+      let that = this;
+      if (that.number >= 0) {
+        await that.contract.methods
+          .store(that.number + 1)
+          .send({ from: that.accounts });
+        that.number = Number(await that.contract.methods.retrieve().call());
+      }
+    },
+    async reduce() {
+      let that = this;
+      if (that.number > 0) {
+        await that.contract.methods
+          .store(that.number - 1)
+          .send({ from: that.accounts });
+        that.number = Number(await that.contract.methods.retrieve().call());
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+button{
+  display: block;
+  width: 100px;
+  height: 50px;
+  margin: 10px auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+div{
+  margin: 10px auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
 </style>
